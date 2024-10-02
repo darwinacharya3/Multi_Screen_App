f79860a15d4aa8a8558ae3a98f4c6b94e6afdfa2
darwinacharya3 <acharyadarwin3@gmail.com>
2024-10-03 02:45:37 +0545

    building multiscreen app

---
+++ meals/lib/widgets/category_grid_item.dart
@@ -0,0 +1,43 @@
+//not manage any internal state so stateless widget
+
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(
+      {super.key,
+      required this.category,
+      required this.onSelectCategory}); //named and positional parameter
+
+  final Category category;
+  final Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ), // ! is to tell the dart that the titlelarge will be defined
+      ),
+    );
+  }
+}
